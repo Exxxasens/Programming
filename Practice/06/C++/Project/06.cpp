48c907c188bd8b7968f7dd6bccd4ae5907540b85
Oleg Okatev <oleg-okatev@hotmail.com>
2020-10-19 07:27:41 +0300

    c++ 06 task fixed

--- Practice/06/C++/Project/06.cpp
+++ Practice/06/C++/Project/06.cpp
@@ -10,13 +10,24 @@ int main() {
 
     cin >> a >> b >> c;
 
-    if (a == 0 && b == 0) {
+    if ((a == 0 && b == 0) || (a == 0 && c == 0) || (b == 0 && c == 0)) {
         cout << "Ошибка ввода" << endl;
     } else if (a == 0) { // линейное уравнение
         cout << -(c / b) << endl;
     } else if (c == 0) {
         cout << 0 << endl;
         cout << b / a << endl;
+    }
+    else if (b == 0) {
+        double result = -c / a;
+        if (result >= 0) {
+            result = sqrt(result);
+            cout << "x1 = " << result << endl;
+            cout << "x2 = " << -result << endl;
+        }
+        else {
+            cout << "Уравнение не имеет действительных корней" << endl;
+        }
     }
     else {
         double d = b * b - 4 * a * c;
