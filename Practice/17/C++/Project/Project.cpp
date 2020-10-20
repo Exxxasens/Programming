8670478c1cca11eb53467b7024143c7f917e98a6
Oleg <oleg-okatev@bk.ru>
2020-10-20 13:30:39 +0300

    17 task c++

--- Practice/17/C++/Project/Project.cpp
+++ Practice/17/C++/Project/Project.cpp
@@ -13,15 +13,35 @@ bool has(std::vector <int> array, int n) {
 }
 
 int main() {
-    
     int numbers[S] = {};
     std::vector <int> lastNums; // last nums for k games;
     int input;
+    int gameCounter = 0, black = 0, red = 0;;
 
     do {
         std::cin >> input;
+
+        if (!(input >= 0 && input <= 36)) {
+            return 0;
+        }
+
         numbers[input]++;
+        gameCounter++;
         lastNums.push_back(input);
+
+
+
+        if (gameCounter > 12) {
+            gameCounter = 0;
+            lastNums.clear();
+            black = 0;
+            red = 0;
+        }
+
+        if (input % 2 == 0) {
+            black++;
+        }
+        else red++;
 
         int f = 0;
 
@@ -37,13 +57,20 @@ int main() {
             }
         }
 
-        for (int i = 0; i < S; i++) {
-            if (has() {
+        std::cout << std::endl;
 
+        for (int i = 0; i < S; i++) {
+            if (!has(lastNums, numbers[i])) {
+                std::cout << i << ' ';
             }
         }
 
-    } while (input >= 0 && input <= 36);
+        std::cout << std::endl;
+        std::cout << red << ' ' << black << std::endl;
+
+
+
+    } while (true);
 
 
 }
