f341e3a1a8d2fda189b4d8e3ec92886100448773
Oleg <oleg-okatev@bk.ru>
2020-10-20 12:57:21 +0300

    16 task c++, half 17 task

---
+++ Practice/16/C++/16/16.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+int main() {
+    int count;
+    std::cin >> count;
+    std::vector <std::string> tickets;
+
+    for (int i = 0; i < count; i++) {
+        std::string ticket;
+        std::cin >> ticket;
+        if (ticket[0] == 'a' && ticket.substr(4) == "55661") {
+            tickets.push_back(ticket);
+        }
+    }
+
+    int ticketsLength = tickets.size();
+
+    if (ticketsLength == 0) {
+        std::cout << -1;
+        return 0;
+    }
+
+    for (int i = 0; i < ticketsLength; i++) {
+        std::cout << tickets[i] << ' ';
+    }
+
+}
---
+++ Practice/17/17/17.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+
+const int K = 12;
+const int S = 37;
+
+bool has(std::vector <int> array, int n) {
+    int length = array.size();
+    for (int i = 0; i < length; i++) {
+        if (array[i] == n) return true;
+    }
+    return false;
+}
+
+int main() {
+    
+    int numbers[S] = {};
+    std::vector <int> lastNums; // last nums for k games;
+    int input;
+
+    do {
+        std::cin >> input;
+        numbers[input]++;
+        lastNums.push_back(input);
+
+        int f = 0;
+
+        for (int i = 0; i < S; i++) {
+            if (numbers[i] > f) {
+                f = numbers[i];
+            }
+        }
+
+        for (int i = 0; i < S; i++) {
+            if (f == numbers[i]) {
+                std::cout << i << ' ';
+            }
+        }
+
+        for (int i = 0; i < S; i++) {
+            if (has() {
+
+            }
+        }
+
+    } while (input >= 0 && input <= 36);
+
+
+}
